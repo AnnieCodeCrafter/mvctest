5bdc9aa3e2861007c9cf763fc0d096254e5950bf
Anouk <ambrondijk@gmail.com>
2020-03-19 09:54:27 +0100

    First

---
+++ TestMVC/Data/ExampleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TestMVC.Data
+{
+    public class ExampleModel
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string Name { get; set; }
+
+       
+
+
+    }
+}
